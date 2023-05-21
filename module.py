fd5bb5519957c560c9873b3443c05ecd057a0bda
zookeeper1234 <75436345+zookeeper1234@users.noreply.github.com>
2023-05-21 21:27:02 +0800

    Add files via upload

---
+++ module.py
@@ -0,0 +1,10 @@
+# def test(a, b):
+#     return a + b
+#
+# if '--name--' == '--main--':
+#     print(test(3, 2))
+__all__ = ['test_A']
+def test_A():
+    print('testA')
+def test_B():
+    print('testB')
